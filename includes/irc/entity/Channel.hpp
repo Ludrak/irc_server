e5fa1188335d236342b3fdc42ea8de70369dcbd2
Nathan <maximenathan@hotmail.fr>
2021-11-11 01:29:18 +0100

    add creator field to Channel

--- includes/irc/entity/Channel.hpp
+++ includes/irc/entity/Channel.hpp
@@ -7,6 +7,8 @@
 
 # define NB_CHANNEL_REGISTRATION_MAX 12
 
+# define M_MODERATED		1 << 1
+
 class Channel : public AEntity, public ASockHandler
 {
 
@@ -19,6 +21,8 @@ class Channel : public AEntity, public ASockHandler
 		Channel &		operator=( Channel const & rhs );
 		
 		uint				getType( void ) const;
+		Client				*getCreator(void);
+		void				setCreator(Client & client);
 
 		uint				addClient(Client & client);
 		uint				removeClient(Client & client);
@@ -26,7 +30,9 @@ class Channel : public AEntity, public ASockHandler
 		void				broadcastPackage(Package & pkg, const SockStream * except = NULL);
 		void            	delSocket(const SockStream &sock);
 
+		//TODO := check for passing M_REGISTRATION_LIMITED to AEntity beacause used by Client too and add a test on this flag before sayin 'cannot inc or dec'
 	private:
+		Client*					_creator; //REVIEW Check where to use it
 		std::list<Client *> 	_clients;
 		std::list<Client *> 	_operators;
 
--- srcs/class/irc/entity/Channel.cpp
+++ srcs/class/irc/entity/Channel.cpp
@@ -4,7 +4,7 @@
 ** ------------------------------- CONSTRUCTOR --------------------------------
 */
 
-Channel::Channel(const std::string & channelName) : AEntity(NB_CHANNEL_REGISTRATION_MAX), ASockHandler()
+Channel::Channel(const std::string & channelName) : AEntity(NB_CHANNEL_REGISTRATION_MAX), ASockHandler(), _creator(NULL)
 {
 	this->setNickname(channelName);
 }
@@ -77,6 +77,8 @@ uint					Channel::removeClient(Client & client)
 		Logger::critical("client registration should already be tested here");
 	this->_clients.remove(&client);
 	this->delSocket(client.getStream());
+	if (this->_creator == &client)
+		this->_creator = NULL;
 	Logger::info("<" + client.getNickname() + "> leave channel <" + this->getNickname() + ">");
 	return SUCCESS;
 }
@@ -87,7 +89,7 @@ void            		Channel::delSocket(const SockStream &sock)
 }
 
 
-void					Channel::broadcastPackage(Package & pkg, const SockStream * except)
+void						Channel::broadcastPackage(Package & pkg, const SockStream * except)
 {
 	for (std::map<ushort, SockStream*>::iterator it = this->_sockets.begin(); it != this->_sockets.end(); ++it)
 	{
@@ -100,7 +102,7 @@ void					Channel::broadcastPackage(Package & pkg, const SockStream * except)
 	}
 }
 
-bool				Channel::isRegistered(Client & client)
+bool					Channel::isRegistered(Client & client)
 {
 	for (std::list<Client *>::iterator it = this->_clients.begin(); it != this->_clients.end(); ++it)
 	{
@@ -110,6 +112,7 @@ bool				Channel::isRegistered(Client & client)
 	return false;
 }
 
+
 /*
 ** --------------------------------- ACCESSOR ---------------------------------
 */
@@ -118,5 +121,16 @@ uint					Channel::getType( void ) const
 {
 	return AEntity::value_type_channel;
 }
+
+Client*					Channel::getCreator( void )
+{
+	return this->_creator;
+}			
+
+void					Channel::setCreator(Client & client)
+{
+	this->_creator = &client;
+}			
+
 
 /* ************************************************************************** */
