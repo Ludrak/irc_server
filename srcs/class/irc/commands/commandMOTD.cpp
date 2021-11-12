e20743f5ede080b6498ff11fc252c80aeca4c084
Nathan <maximenathan@hotmail.fr>
2021-11-12 04:31:44 +0100

    add files for motd and rules

---
+++ srcs/class/irc/commands/commandMOTD.cpp
@@ -0,0 +1,8 @@
+# include "IRCServer.hpp"
+
+uint						IRCServer::_commandMOTD(Client & client, std::string cmd)
+{
+	//TODO implement MOTD command
+	(void)client;
+	(void)cmd;
+}
---
+++ srcs/class/irc/commands/commandVERSION.cpp
@@ -0,0 +1,11 @@
+# include "IRCServer.hpp"
+
+uint						IRCServer::_commandVERSION(Client & client, std::string cmd)
+{
+	//TODO implement VERSION command
+	(void)client;
+	(void)cmd;
+	// if (Paser::nbParam(cmd) == 0)
+	// 	return this->_reply(RPL_VERSION);
+	// std::string target = Arser::getParam()
+}
