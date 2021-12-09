de1813f27fee52d6fa69c49504f86354ae8cae0a
Ludrak <luca06.robino@gmail.com>
2021-12-09 22:35:35 +0100

    Logger update: added defines in Logger.hpp to define colors and text modes for logs, using ANSI rgb

--- includes/utils/Logger.hpp
+++ includes/utils/Logger.hpp
@@ -9,7 +9,7 @@
 # include <sys/time.h>
 # include "ntos.hpp"
 
-
+// define basic ansi colors
 # define RESET_ANSI		"\033[0m"		
 # define BOLD_ANSI		"\033[1m"		
 # define RED_ANSI		"\033[91m"		
@@ -19,12 +19,40 @@
 # define GREEN_ANSI		"\033[92m"		
 # define DBLUE_ANSI		"\033[94m"		
 
-# define CORE		5
-# define DEBUG      4
-# define INFO       3
-# define WARNING    2
-# define ERROR      1
-# define CRITICAL	0
+// define prefix id's 
+# define CORE		    5
+# define DEBUG          4
+# define INFO           3
+# define WARNING        2
+# define ERROR          1
+# define CRITICAL	    0
+
+// define prefixes RGB's here
+# define CORE_COLOR		"\033[38;2;240;240;220m"
+# define DEBUG_COLOR    "\033[38;2;180;210;255m\033[3m"
+# define INFO_COLOR     "\033[38;2;150;250;105m"
+# define WARNING_COLOR  "\033[38;2;250;230;15m\033[3m"
+# define ERROR_COLOR    "\033[38;2;255;67;33m\033[1m"
+# define CRITICAL_COLOR	"\033[48;2;250;100;20m\033[1m"
+
+// define timestamp color here
+# define TIMESTAMP_COLOR    "\033[38;2;100;100;120m"
+
+// define message output color here
+# define CORE_MSG_COLOR     "\033[38;2;210;210;190m"
+# define DEBUG_MSG_COLOR    "\033[38;2;180;210;255m"
+# define INFO_MSG_COLOR     "\033[38;2;230;230;210m"
+# define WARNING_MSG_COLOR  "\033[38;2;250;230;40m"
+# define ERROR_MSG_COLOR    "\033[38;2;255;77;50m"
+# define CRITICAL_MSG_COLOR "\033[38;2;250;190;80m"
+
+// define prefix structure here
+# define CORE_LOG		"["CORE_COLOR"CORE"RESET_ANSI"]    | "CORE_MSG_COLOR"\033[3m"
+# define DEBUG_LOG      "["DEBUG_COLOR"DEBUG"RESET_ANSI"]   | "DEBUG_MSG_COLOR"\033[3m"
+# define INFO_LOG       "["INFO_COLOR"INFO"RESET_ANSI"]    | "INFO_MSG_COLOR
+# define WARNING_LOG    "["WARNING_COLOR"WARN"RESET_ANSI"]    | "WARNING_MSG_COLOR"\033[3m"
+# define ERROR_LOG      "["ERROR_COLOR"ERROR"RESET_ANSI"]   | "ERROR_MSG_COLOR
+# define CRITICAL_LOG	"["CRITICAL_COLOR"CRITICAL"RESET_ANSI"]| "CRITICAL_MSG_COLOR
 
 typedef unsigned int uint;
 
@@ -35,7 +63,6 @@ class Logger
 
 		static time_t		getInitialTimestamp( void );
 		static std::string	getTimestamp( void );
-		static std::string	getTimestampOffset( void );
         static uint			getLogLevel( void );
         static void			setLogLevel(uint level);
 
--- srcs/utils/Logger.cpp
+++ srcs/utils/Logger.cpp
@@ -26,28 +26,14 @@ std::string	Logger::getTimestamp( void )
 
     gettimeofday(&time_now, nullptr);
 	timeinfo = localtime(&time_now.tv_sec);
-	strftime(buffer,sizeof(buffer),"%d-%m-%Y %H:%M:%S",timeinfo);
+	strftime(buffer,sizeof(buffer), "%d-%m-%Y %H:%M:%S", timeinfo);
 	std::string ms(ntos(time_now.tv_usec));
 	ms.resize(3);
-	ss << "[" << buffer << ":" << ms << "]";
+	ss << "["TIMESTAMP_COLOR << buffer << ":" << ms << RESET_ANSI"]";
 	return ss.str();
 
 }
 
-
-std::string	Logger::getTimestampOffset( void )
-{
-	//REVIEW optimisable par sauvegarde de valeur (par exemple recalculer la string des heures seulement lorsque les minutes reviennent à 0) (à creuser)
-	double	time = std::difftime(std::time(NULL), Logger::_initTime);
-	uint	seconds = static_cast<uint>(time);
-	uint	minutes = (seconds / 60) % 60;
-	uint	hours = (seconds / 3600) % 24;
-	uint	days = (seconds / 86400);
-	seconds = seconds % 60;
-	std::stringstream ss;
-	ss << std::setfill('0') << "[" << days << "D" << std::setw(2) << hours << "h" << std::setw(2) << minutes << ":" << std::setw(2) << seconds << "]";
-	return ss.str();
-}
 
 
 void		Logger::log( uint level, const std::string &message )
@@ -88,35 +74,35 @@ void	Logger::setLogLevel(uint level)
 void    Logger::core( const std::string &message )
 {
 	if (Logger::logLevel >= CORE )
-	    std::cout << Logger::getTimestamp() << "[" << DBLUE_ANSI << "CORE" << RESET_ANSI << "] | " << message << std::endl;
+	    std::cout << Logger::getTimestamp() << CORE_LOG << message << RESET_ANSI"\033[23m" << std::endl;
 }
 
 void    Logger::debug( const std::string &message )
 {
 	if (Logger::logLevel >= DEBUG )
-	    std::cout << Logger::getTimestamp() << "[" << LGREY_ANSI << "DEBUG" << RESET_ANSI << "]| " << message << std::endl;
+	    std::cout << Logger::getTimestamp() << DEBUG_LOG << message << RESET_ANSI"\033[23m" << std::endl;
 }
 
 void    Logger::info( const std::string &message )
 {
 	if (Logger::logLevel >= INFO )
-    	std::cout << Logger::getTimestamp() << "[" << GREEN_ANSI << "INFO" << RESET_ANSI << "] | " << message << std::endl;
+    	std::cout << Logger::getTimestamp() << INFO_LOG << message  << RESET_ANSI << std::endl;
 }
 
 void    Logger::warning( const std::string &message )
 {
 	if (Logger::logLevel >= WARNING )
- 	   std::cerr << Logger::getTimestamp() << "[" << YELLOW_ANSI << "WARN" << RESET_ANSI << "] | " << YELLOW_ANSI << message << RESET_ANSI << std::endl;
+ 	   std::cerr << Logger::getTimestamp() << WARNING_LOG << message << RESET_ANSI"\033[23m" << std::endl;
 }
 
 void    Logger::error( const std::string &message )
 {
 	if (Logger::logLevel >= ERROR )
-    	std::cerr << Logger::getTimestamp() << "[" << RED_ANSI << "ERROR" << RESET_ANSI << "]| " << RED_ANSI << message << RESET_ANSI << std::endl;
+    	std::cerr << Logger::getTimestamp() << ERROR_LOG << message << RESET_ANSI << std::endl;
 }
 
 void    Logger::critical( const std::string &message )
 {
 	if (Logger::logLevel >= CRITICAL )
-	    std::cerr << Logger::getTimestamp() << RED_ANSI << "\033[48;2;255;165;0m[" << DBLUE_ANSI << "CRITICAL" << RED_ANSI << "]" << RESET_ANSI << " " << "\033[38;2;255;165;0m" << message << RESET_ANSI << std::endl;
+	    std::cerr << Logger::getTimestamp() << CRITICAL_LOG << message << RESET_ANSI << std::endl;
 }
